f1085c330a2a81e5f1be65249074621679326095
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-12-14 17:18:08 +0200

    Update RestoreCode.jsx

--- src/pages/RestoreCode/RestoreCode.jsx
+++ src/pages/RestoreCode/RestoreCode.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LoginContainer } from '../Login/Login.styled'
 import { LoginBanner } from '../Login/components/LoginBanner/LoginBanner'
 import { LoginMain } from '../Login/components/LoginMain/LoginMain'
@@ -6,8 +6,29 @@ import { LoginTitle } from '../../components/LoginTitle/LoginTitle'
 import { LoginFormStyle } from '../Login/components/LoginForm/LoginForm.styled'
 import { ButtonForm } from '../../components/ButtonForm/ButtonForm'
 import { InputVerification } from '../../components/InputVerification/InputVerification'
+import { useNavigate } from 'react-router-dom'
 
 export const RestoreCode = () => {
+    const [setVerification] = useState(['', '', '', '', '', '',])
+    const [status] = useState('success');
+    const navigate = useNavigate();
+
+    const submitForm = (evt) => {
+        evt.preventDefault();
+        setTimeout(() => {
+            setVerification((prevVerification) => {
+                if (status === 'success') {
+                    console.log('code for restore pass success', { prevVerification });
+                    navigate('/new-password')
+                } else if (status === 'wrong_pass') {
+                    console.log('your pass is wrong!');
+                    // сюда функц с красніми полями
+                }
+                return prevVerification;
+            });
+        }, 1000);
+    };
+
 
   return (
     <div className="container-main-pages">
@@ -17,7 +38,7 @@ export const RestoreCode = () => {
 
                 <LoginTitle arrow={true} title={'Enter Code'} desc={"We've sent an OTP verification code to your email. Kindly input it here."}/>
 
-                <LoginFormStyle >
+                <LoginFormStyle onSubmit={submitForm}>
 
                     <div className="form__input__buttons form__input__buttons_mar">
                         
