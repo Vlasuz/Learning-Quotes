a7e257d616161c79c5b40a5311b7edc4b33df7ec
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-01-03 16:37:02 +0200

    areaStats start

---
+++ src/pages/Area/components/AreaStats/AreaStats.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AreaStatsStyle } from './AreaStats.styled'
+
+export const AreaStats = () => {
+  return (
+    <AreaStatsStyle>
+        <h2>
+            Anna
+        </h2>
+        <a href="mailto:your.email@gmail.com">
+            your.email@gmail.com
+        </a>
+        <h3>
+            Status: <span>StatusName</span>
+        </h3>
+        <h3>
+            Study Time: <span>Avg 2 h/day Avd 10h/week</span>
+        </h3>
+        <button>
+            Buy Premium
+        </button>
+    </AreaStatsStyle>
+  )
+}
