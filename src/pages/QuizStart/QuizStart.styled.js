c382fc7c09d25989fb91a9015798139cbdcc036f
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-01-04 15:35:47 +0200

    Update QuizStart.styled.js

--- src/pages/QuizStart/QuizStart.styled.js
+++ src/pages/QuizStart/QuizStart.styled.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import PageBgd from '../../assets/img/pageBgd.jpg'
+
 export const QuizStartStyle = styled.div`
-    
+    margin: 32px 0 56px;
+    background: url(${PageBgd}) no-repeat;
+    background-size: cover;
+    padding: 46px 45px 86px;
 `
