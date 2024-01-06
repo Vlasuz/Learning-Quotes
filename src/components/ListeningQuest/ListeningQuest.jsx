fac423ffc9b362a4690547a825af7c32eab7c4db
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-01-06 17:57:31 +0200

    create component

---
+++ src/components/ListeningQuest/ListeningQuest.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { QuizPageStyle } from '../../pages/QuizPage/QuizPage.styled'
+import { BackBtn } from '../BackBtn/BackBtn'
+import { QuizTitle } from '../QuizTitle/QuizTitle'
+import { QuizAudio } from '../QuizAudio/QuizAudio'
+
+import AudioImg from '../../assets/img/listening.png'
+
+export const ListeningQuest = () => {
+  return (
+    <QuizPageStyle>
+
+        <BackBtn link={'/some'}/>
+
+        <QuizTitle quizName={'DLPT 2 +.'} quizTxt={'Listen this audio'} />
+
+        <img className='audio__img' src={AudioImg} alt="audio ph" />
+
+        <QuizAudio />
+
+    </QuizPageStyle>
+  )
+}
--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import { QuizPageStyle } from './QuizPage.styled'
-import { BackBtn } from '../../components/BackBtn/BackBtn'
-import { QuizTitle } from '../../components/QuizTitle/QuizTitle'
-import { QuizAudio } from '../../components/QuizAudio/QuizAudio'
 
-import AudioImg from '../../assets/img/listening.png'
+import { ListeningQuest } from '../../components/ListeningQuest/ListeningQuest'
 
 export const QuizPage = () => {
   return (
     <div className='container-login'>
         
-        <QuizPageStyle>
-
-            <BackBtn link={'/some'}/>
-
-            <QuizTitle quizName={'DLPT 2 +.'} quizTxt={'Listen this audio'} />
-
-            <img className='audio__img' src={AudioImg} alt="audio ph" />
-
-            <QuizAudio />
-
-        </QuizPageStyle>
+        <ListeningQuest/>
+        
     </div>
   )
 }
