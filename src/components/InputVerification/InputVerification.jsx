39f0fbfe1df755f7069171a307dfc67340b6051d
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-01-11 21:50:20 +0200

    Update InputVerification.jsx

--- src/components/InputVerification/InputVerification.jsx
+++ src/components/InputVerification/InputVerification.jsx
@@ -1,49 +1,45 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
 
 export const InputVerification = () => {
-    const [verification, setVerification] = useState(['', '', '', '', '', '',])
+    const [verification, setVerification] = useState(['', '', '', '', '', '']);
+    const [inputValue, setInputValue] = useState('');
     const inputRefs = useRef(Array(6).fill(null).map(() => React.createRef()));
 
-    const inputChange = (index, value) => {
+    const inputChange = (index, key) => {
+        console.log(index);
         const newVerification = [...verification];
-        newVerification[index] = value;
+        newVerification[index] = inputValue;
         setVerification(newVerification);
 
-        // move focus to the next input
-        if (value !== '' && index < inputRefs.current.length - 1) {
-            inputRefs.current[index + 1].current.focus();
+        if ( key === 'Backspace') {
+            inputRefs.current[index - 1]?.current.focus();
+        } else {
+            inputRefs.current[index + 1]?.current.focus();
         }
-    };
-
-    const inputBackspace = (index, evt) => {
-        // move focus to the previous input on backspace
-        if (evt.key === 'Backspace' && index > 0) {
-            inputRefs.current[index - 1].current.focus();
-        };
     }
 
-  return (
-    <>
-        <label className='verification__label' htmlFor='text'>Code</label>
-        <div className="verification__inner">
-            {verification.map((digit, index) => (    
-                <input className='verification__input'
-                    key={index}
-                    type="text"
-                    ref={inputRefs.current[index]}
-                    maxLength='1'
-                    value={digit}
-                    onChange={(evt) => inputChange(index, evt.target.value)} 
-                    onKeyDown={(evt) => inputBackspace(index, evt)}
-                />
-            ))}
-        </div>
+    return (
+        <>
+            <label className='verification__label' htmlFor='text'>Code</label>
+            <div className="verification__inner">
+                {verification.map((digit, index) => (
+                    <input className='verification__input'
+                        key={index + digit}
+                        type="text"
+                        ref={inputRefs.current[index]}
+                        maxLength='1'
+                        value={digit}
+                        onChange={(evt) => setInputValue(evt.target.value)}
+                        onKeyUp={(evt) => inputChange(index, evt.key)}
+                    />
+                ))}
+            </div>
             <div className="verification__again">
                 <p>
-                    will expired in <span>00:55</span>
+                    will expire in <span>00:55</span>
                 </p>
                 <button>Send Again</button>
             </div>
-    </>
-  )
-}
+        </>
+    );
+};
