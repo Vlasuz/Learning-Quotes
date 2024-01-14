e6ee4c3bf48f32d68bed31970da4c6e27d163d1b
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-01-14 23:40:45 +0200

    create TutorAreaOwner

--- src/pages/Area/components/AreaOwner/AreaOwner.jsx
+++ src/pages/Area/components/AreaOwner/AreaOwner.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AreaOwnerStyle } from './AreaOwner.styled'
+// import getCookie from '../../../../functions/getCookie'
+// import setCookie from '../../../../functions/setCookie'
 
 import Owner from '../../../../assets/img/owner1.png'
+import ArrowIc from '../../../../assets/img/icons/Arrow-top-desk.svg'
 
 export const AreaOwner = () => {
+  const [tutorPopUp, setTutorPopUp] = useState(false);
+
+
+  useEffect(() => {
+    // const hasCookie = getCookie('tutorial__owner') === 'true';
+    const tutorTimeout = setTimeout(() => {
+      setTutorPopUp(true);
+    }, 2000)
+
+    // if (!hasCookie) {
+      // setTutorPopUp(true);
+      // setCookie('tutorial__owner', 'true');
+    // }
+
+    return () => clearTimeout(tutorTimeout);
+  }, []);
+
   return (
     <AreaOwnerStyle>
       <img src={Owner} alt="owner ph" />
+
+      {tutorPopUp && (
+        <div className="tutorial__owner">
+          <img className='tutorial__owner__arrow' src={ArrowIc} alt="arrow ic" />
+          <h2>
+            New Arrival
+          </h2>
+          <p>
+            You've just arrived here; you're unknown, your skills and your worth are a big mystery to everyone.
+          </p>
+        </div>
+      )}
     </AreaOwnerStyle>
   )
 }
--- src/pages/Area/components/AreaOwner/AreaOwner.styled.js
+++ src/pages/Area/components/AreaOwner/AreaOwner.styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const AreaOwnerStyle = styled.div`
+    position: relative;
+
     img{
         margin-top: 20px;
         max-width: 270px;
@@ -8,6 +10,46 @@ export const AreaOwnerStyle = styled.div`
 
         @media screen  and (max-width: 768px){
             max-width: 200px;
+        }
+    }
+
+    .tutorial__owner{
+        padding: 12px;
+        border-radius: 8px;
+        background: #FDE7B6;
+        width: 310px;
+        position: absolute;
+        bottom: -33px;
+        right: -56px;
+        z-index: 8;
+
+        h2{
+            color: #000;
+            text-align: center;
+            font-size: 20px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 16px;
+            margin-bottom: 15px;
+        }
+
+        p{
+            color: #000;
+            text-align: center;
+            font-size: 12px;
+            font-style: normal;
+            font-weight: 700;
+            line-height: 16px;
+
+        }
+
+        &__arrow{
+            width: 16px;
+            height: 8px;
+            position: absolute;
+            top: -27px;
+            left: 50%;
+            transform: translate(-50%);
         }
     }
 `
