b616e63aaac7df50a39d5708d6b051c248d939fb
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-03-21 16:34:40 +0200

    del login

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -7,27 +7,34 @@ import { LoginFormStyle } from '../Login/components/LoginForm/LoginForm.styled'
 import { Input } from '../../components/Input/Input'
 import { ButtonForm } from '../../components/ButtonForm/ButtonForm'
 import { NavLink, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { getApiLink } from '../../api/getApiLink'
 
 export const SignUp = () => {
     const [pass, setPass] = useState('');
-    const [repeatPass, setRepeatPass] = useState('');
+    // const [repeatPass, setRepeatPass] = useState('');
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
-    const [status] = useState('success');
+    // const [status] = useState('success');
     const navigate = useNavigate();
-
+    
+    const resetForm = () => {
+        setEmail('');
+        setPass('');
+    }
+    
     const submitForm = (evt) => {
         evt.preventDefault();
-        setTimeout(() => {
-            if (status === 'success') {
-                // resetForm();
-                console.log('email for restore success', { name, email, pass, repeatPass });
-                navigate('/sign-up-verify')
-            } else if (status === 'wrong_pass') {
-                console.log('your pass is wrong!');
-                // сюда функц с красніми полями
-            }
-        }, 1000); 
+
+        axios.post(getApiLink(`/api/auth/sign_up?name=${name}&email=${email}&password=${pass}`))
+            .then(({data}) => {
+                console.log(data);
+                resetForm();
+                navigate('/choose-lang')
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
   return (
     <div className="container-main-pages">
@@ -40,7 +47,7 @@ export const SignUp = () => {
                 <Input type={'text'} label={'Name'} inputValue={setName} placeholder={'Enter your Name'}/>
                 <Input type={'email'} label={'Email Address'} inputValue={setEmail} placeholder={'Enter your Email'}/>
                 <Input type={'password'} label={'Enter Password'} inputValue={setPass} placeholder={'Enter your Password'}/>
-                <Input type={'password'} label={'Repeat Password'} inputValue={setRepeatPass} placeholder={'Repeat your Password'}/>
+                {/* <Input type={'password'} label={'Repeat Password'} inputValue={setRepeatPass} placeholder={'Repeat your Password'}/> */}
 
                 <div className="form__input__buttons form__input__buttons_mar">
 
