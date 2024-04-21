9e9a4e0740bf8b7137c1d02654feba3cd24f1cdd
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-04-21 13:29:45 +0300

    update images

--- src/pages/Area/components/AreaPrize/AreaPrize.jsx
+++ src/pages/Area/components/AreaPrize/AreaPrize.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AreaPrizeStyle } from './AreaPrize.styled'
 
-// import Cup from '../../../../assets/img/icons/cup.svg'
+import Cup from '../../../../assets/img/icons/cup.svg'
 import Shelf1 from '../../../../assets/img/shelf1.png'
 import Shelf2 from '../../../../assets/img/shelf2.png'
 
@@ -10,11 +10,11 @@ export const AreaPrize = () => {
     <AreaPrizeStyle className='animate__animated animate__fadeInLeft'>
         <ul className='shelf__list'>
             <li>
-                {/* <img className='shelf__list__cup' src={Cup} alt="cup ph" /> */}
+                <img className='shelf__list__cup' src={Cup} alt="cup ph" />
                 <img className='shelf__list__polka' src={Shelf1} alt="shelf ph" />
             </li>
             <li>
-                {/* <img className='shelf__list__cup' src={Cup} alt="cup ph" /> */}
+                <img className='shelf__list__cup' src={Cup} alt="cup ph" />
                 <img className='shelf__list__polka' src={Shelf2} alt="shelf ph" />
             </li>
         </ul>
